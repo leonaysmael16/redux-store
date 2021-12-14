b67cb2faa9b3fe2aa1a96b1177ddaf4bb92d245c
leonaysmael16 <ysmealleona@gmail.com>
2021-12-14 00:24:30 -0800

    deploy

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-// import { useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import { useSelector, useDispatch } from 'react-redux';
-import { useQuery } from '@apollo/react-hooks';
+// import { useQuery } from '@apollo/react-hooks';
 import Cart from '../components/Cart';
 // import { useStoreContext } from '../utils/GlobalState';
 import {
