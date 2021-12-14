bb8330ea247b9212204ea26da6960e5809d1a860
leonaysmael16 <ysmealleona@gmail.com>
2021-12-13 20:47:36 -0800

    refactor

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import ProductItem from '../ProductItem';
 // import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
-import { useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/react-hooks';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 import spinner from '../../assets/spinner.gif';
